a8a2fd0ec982a90744b219cdf983d9dedabbcfa3
solderq35 <solderq35@gmail.com>
2022-11-10 12:05:53 -0800

    update delete function

--- register_commands/delete.js
+++ register_commands/delete.js
@@ -1,10 +1,17 @@
 const { REST, Routes } = require("discord.js");
-const { clientId, token } = require("./config.json");
+const { clientId, token, guildId } = require("./config.json");
 
 const rest = new REST({ version: "10" }).setToken(token);
 
+// for guild-based commands
+rest.delete(Routes.applicationGuildCommand(clientId, guildId, '1040233500021882920'))
+	.then(() => console.log('Successfully deleted guild command'))
+	.catch(console.error);
+
+/*
 // for global commands
 rest
   .delete(Routes.applicationCommand(clientId, "1040209415254593596"))
   .then(() => console.log("Successfully deleted application command"))
   .catch(console.error);
+*/
